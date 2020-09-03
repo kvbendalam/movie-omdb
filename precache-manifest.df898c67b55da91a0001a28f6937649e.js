621db16a4bf3cf1e55932770b43372c56a769e38
krishnavamsi Bendalam <krishnavamsi@krishnavamsis-MacBook-Air.local>
2020-09-03 10:44:52 +0530

    Updates

---
+++ precache-manifest.df898c67b55da91a0001a28f6937649e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c23005ce3e17169c923255b7e0b21c",
+    "url": "/movie-omdb/index.html"
+  },
+  {
+    "revision": "aeb0d10391ffff76f5e4",
+    "url": "/movie-omdb/static/css/main.1fecff57.chunk.css"
+  },
+  {
+    "revision": "3e6d74492dfb7194aece",
+    "url": "/movie-omdb/static/js/2.a05cde27.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/movie-omdb/static/js/2.a05cde27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeb0d10391ffff76f5e4",
+    "url": "/movie-omdb/static/js/main.8c728727.chunk.js"
+  },
+  {
+    "revision": "012f899b4b852d243510",
+    "url": "/movie-omdb/static/js/runtime-main.ee811e6a.js"
+  }
+]);
